101375661212b7508772ea91ce374ac7db5a1775
ravivasandany <moch.vasandany@gmail.com>
2023-09-29 21:16:42 +0700

    waw

--- storage/logs/log-2023-09-29.php
+++ storage/logs/log-2023-09-29.php
@@ -1,3 +1,5 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 
 ERROR - 2023-09-29 13:36:35 --> Unable to connect to the database
+ERROR - 2023-09-29 14:12:24 --> Unable to connect to the database
+ERROR - 2023-09-29 14:12:26 --> Unable to connect to the database
